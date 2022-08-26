51ed30af39a7c409f24ed710d19f87643960b599
lhernandezd <21372351+lhernandezd@users.noreply.github.com>
2022-08-26 07:13:24 -0500

    Add sliding bar animation

--- public/index.js
+++ public/index.js
@@ -10,6 +10,35 @@ function fetchData() {
     });
 }
 
+function calculateIndicatorPosition() {
+  const currentActiveElement = document.querySelectorAll("a.active")[0];
+  const leftOffset = currentActiveElement.offsetLeft;
+  const width = currentActiveElement.offsetWidth;
+
+  indicator.style.left = `${leftOffset}px`;
+  indicator.style.width = `${width}px`;
+}
+
+function setElementAndIndicatorState(element, initial = false) {
+  const activeLink = document.querySelectorAll("a.active");
+
+  // remove active class from all links
+  activeLink.forEach((item) => {
+    item.classList.remove("active");
+  });
+
+  // add active class to clicked link
+  element.classList.add("active");
+
+  calculateIndicatorPosition();
+
+  // handle initial state
+  if (initial) {
+    element.parentElement.setAttribute("aria-selected", "true");
+    indicator.style.display = "inline-block";
+  }
+}
+
 async function createNavigationElements() {
   const navigation = document.getElementById("navigation-list");
   const { cities } = await fetchData();
@@ -24,13 +53,29 @@ async function createNavigationElements() {
     element.setAttribute("tabindex", "0");
     element.setAttribute("aria-selected", "false");
     elementLink.setAttribute("data-section", `${item.section}`);
+    elementLink.setAttribute("data-timezone", `${item.timezone}`);
     elementLink.href = `#${item.section}`;
     elementLink.innerText = item.label;
+
+    elementLink.addEventListener("click", (e) => {
+      e.preventDefault();
+      setElementAndIndicatorState(e.target);
+    });
 
     // add element to element
     element.appendChild(elementLink);
     navigation.appendChild(element);
   });
+
+  // set initial active state
+  setInitialActiveState();
 }
+
+function setInitialActiveState() {
+  const element = document.querySelector("#navigation-list li:first-child a");
+  if (element) setElementAndIndicatorState(element, true);
+}
+
+const indicator = document.getElementById("navigation-indicator");
 
 createNavigationElements();
